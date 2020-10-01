1204275870d851b11b1da0641c246d9a3ea9ea16
Josh Herrera <joshherrera09@gmail.com>
2020-10-01 11:43:32 -0700

    burger.js test 1 complete

---
+++ models/burger.js
@@ -0,0 +1,25 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+// Object for all our SQL statement functions.
+var burgers = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+
+    insertOne: function(cols, vals, cb) {
+        orm.insertONe("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burgers;
