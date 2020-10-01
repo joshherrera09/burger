7107b3c904fc95783ef6e6e9086dddf67846f228
Josh Herrera <joshherrera09@gmail.com>
2020-10-01 13:39:46 -0700

    views started

---
+++ controllers/burger_controller.js
@@ -0,0 +1,26 @@
+var express = require ("express");
+const burgers = require("../models/burger.js");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+router.get("/", function (req, res) {
+    burger.selectALl(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burgers.create([
+
+    ])
+})
+
+
+module.exports = router;
