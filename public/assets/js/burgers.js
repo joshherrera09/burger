b7332514218114228c8217809df980a400539436
Josh Herrera <joshherrera09@gmail.com>
2020-11-30 15:10:47 -0800

    still need devour button to work

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,39 +1,18 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function(){
-    $(".devour").on("click", function(event) {
-        var id = $(this).data("id");
-
-        // Send the PUT request
-        $.ajax("/api/burgers/" + id, {
-            type: "PUT",
-            data: true
-        }).then(
-            function() {
-            console.log("Devoured = true");
-            // Reload the page to get the updated list
-            location.reload();
-        })
-    })
-
-    $(".create-form").on("submit", function (event) {
-        // Make sure to preventDefault on a submit event.
-        event.preventDefault();
-
-        var newBurger = {
-            burger_name: $("#burgs").val().trim(),
-            // devoured: $("[name=devoured]:checked").val().trim(),
-            devoured: 0 
-        };
-
-        // Send the POST request
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(function() {
-            console.log("Nice, you created a new burger!");
-            // Reload the page to get the updated list
-            location.reload();
-        })
-    })
-})
+$(document).ready(function() {
+    
+    $(".devour-form").on("submit", function(event) {
+      event.preventDefault();
+  
+      var burger_id = $(this).children(".burger_id").val();
+      console.log(burger_id);
+      $.ajax({
+        method: "PUT",
+        url: "/burgers/" + burger_id
+      }).then(function(data) {
+        // reload page to display devoured burger in proper column
+        location.reload();
+      });
+  
+    });
+  });
 
